7e2e7dfb213e3569eeb7fef3d7bb4af729706811
Shaheer Uddin <k230649@nu.edu.pk>
2023-10-31 20:22:33 +0500

    I added commments to the Diff.c File

--- Diff.c
+++ Diff.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int l,m,diff;
+    int l,m,diff; //Initialization of different variables.
     printf("Enter the first number: ");
-    scanf("%d",&l);
+    scanf("%d",&l); //This takes the input of the first number 
     printf("Enter the second number: ");
-    scanf("%d",&m);
+    scanf("%d",&m); //This takes the input of the second number
     diff= l-m;
-    printf("The difference of the two numbers are: %d",diff);
+    printf("The difference of the two numbers are: %d",diff); //This prints the difference of two numbers
 }
