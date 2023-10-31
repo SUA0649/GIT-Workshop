14aa19272ebbfbfd453c7ad45cb54dc0cefa930d
Shaheer Uddin <k230649@nu.edu.pk>
2023-10-31 19:24:08 +0500

    Creation of Diff.c

---
+++ Diff.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int l,m,diff;
+    printf("Enter the first number: ");
+    scanf("%d",&l);
+    printf("Enter the second number: ");
+    scanf("%d",&m);
+    diff= l-m;
+    printf("The difference of the two numbers are: %d",diff);
+}
